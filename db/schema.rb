f8a9774bda02e23caa2ffee478dd79869a7e82d3
MrQile <quitalizner@gmail.com>
2017-07-30 07:45:35 -0700

    Add product Category model and controller with relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730142845) do
+ActiveRecord::Schema.define(version: 20170730144101) do
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.string "speed"
+    t.string "size"
+    t.integer "price"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
